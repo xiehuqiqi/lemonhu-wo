957b1ff5018c8a803e3e3cc23313d9b380cab5f7
lemonhu <shenming26@outlook.com>
2023-02-25 20:31:03 +0800

    no.2 1.0.1

--- git.bat
+++ git.bat
@@ -1,6 +1,5 @@
-git init
 git add *
 git commit -m "frf4dee"
-git branch -M vue
+git branch -M huwo
 git remote add origin git@github.com:xiehuqiqi/lemonhu-wo.git
-git push -u origin vue
+git push -u origin huwo
